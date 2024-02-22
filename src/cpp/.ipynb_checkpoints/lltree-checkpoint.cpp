aaf49476c5a396c869448d06f3b085b6458e23fc
alexandreib <alexandre.durand.spam@gmail.com>
2024-02-22 11:53:31 +0800

    with index and array.

--- src/cpp/.ipynb_checkpoints/lltree-checkpoint.cpp
+++ src/cpp/.ipynb_checkpoints/lltree-checkpoint.cpp
@@ -8,91 +8,14 @@
 #include "node.hpp"
 #include "criterion.hpp"
 
+#include <chrono>
 
 class lltree{
 private :
-    int max_depth;
+    int max_depth, number_of_cols;
     long unsigned int min_size_split;
     node node_0;
     criterion tree_criterion;
-
-    void _grow(node* pnode, std::vector<std::vector<double>> XY) {
-        bool depth = (lltree::max_depth >= (pnode->tree_level + 1));
-        bool msamp = (lltree::min_size_split <= (XY.size()));
-        
-        size_t index_col_Y = XY[0].size() - 1;
-        size_t n_rows = XY.size();
-        
-        if (depth && msamp) {    
-            for (size_t index_col = 0; index_col < index_col_Y; index_col ++) {
-                std::vector<double> col;
-                for (size_t index_row = 0; index_row < n_rows; index_row ++){
-                    col.push_back(XY[index_row][index_col]);
-                }
-                
-                std::sort(col.begin(), col.end());
-                col.erase(std::unique(col.begin(), col.end()), col.end());
-
-                for(size_t idx = 0; idx < col.size() - 1; idx++) {
-                    double threshold = (col[idx] + col[idx + 1])/2;
-                    std::vector<double> l_Y, r_Y;
-                    for (size_t index_row = 0; index_row < n_rows; index_row ++){
-                        if (XY[index_row][index_col] < threshold) {
-                            l_Y.push_back(XY[index_row][index_col_Y]);
-                        }
-                        else {
-                            r_Y.push_back(XY[index_row][index_col_Y]);
-                        }
-                    }
-                    double dj = double(idx);
-                    double loss = (dj/(double) n_rows)*impurity(l_Y) + (((double) n_rows - dj)/(double) n_rows)*impurity (r_Y);
-                    if ((loss < pnode->saved_loss) && (r_Y.size() >= lltree::min_size_split) && (l_Y.size() >= lltree::min_size_split)) {
-                        pnode->saved_loss = loss;
-                        pnode->saved_col_index = index_col;
-                        pnode->saved_threshold = threshold;
-                        pnode->isleaf = false ;  
-                    }
-                }  
-            }
-        }
-        
-        if (!pnode->isleaf) {
-            std::vector<std::vector<double>> l_XY, r_XY;
-            for (size_t index_row = 0; index_row < n_rows; index_row ++){
-                if (XY[index_row][pnode->saved_col_index] < pnode->saved_threshold) 
-                    {l_XY.push_back(XY[index_row]);}
-                else 
-                    {r_XY.push_back(XY[index_row]);}
-            }
-            node* l_node = new node(pnode->tree_level + 1);
-            node* r_node = new node(pnode->tree_level + 1);
-            pnode->set_children(l_node, r_node);
-            this->_grow(l_node, l_XY);
-            this->_grow(r_node, r_XY);
-        } 
-        else {
-            double average = 0;
-            for (size_t index_row = 0; index_row < n_rows; index_row ++){
-                average = average + XY[index_row][index_col_Y];
-            }                
-            pnode->leaf_value = average / n_rows; 
-            pnode->isleaf = true ;  
-        }  
-        
-    }
-
-    double _traverse(node* pnode, std::vector<double> row) {
-        if (pnode->isleaf){
-            return(pnode->leaf_value);
-        }
-        
-        if (row[pnode->saved_col_index] < pnode->saved_threshold) {
-            return(_traverse(pnode->get_l_children(),row)); 
-        }
-        else {
-            return(_traverse(pnode->get_r_children(),row)); 
-        }
-    }
 
     void _print_tree(node* node) {
         if (!node->isleaf) {
@@ -102,11 +25,113 @@ private :
         else {node->print();}
     }  
 
+    void _grow(node* pnode, double *X , double *Y, const std::vector<int> index) {
+        bool depth = (lltree::max_depth >= (pnode->tree_level + 1));
+        if (depth) {    
+            for (int index_col = 0; index_col < this->number_of_cols; index_col ++) {
+                std::vector<double> col;
+                for(const int& index_row : index) {
+                    col.push_back(X[index_row * this->number_of_cols + index_col]); 
+                }
+                std::sort(col.begin(), col.end());
+                col.erase(std::unique(col.begin(), col.end()), col.end());
+                
+                for(long unsigned int idx = 0; idx < col.size() - 1; idx++) {
+                    double threshold = (col[idx] + col[idx + 1])/2;
+                    std::vector<double> l_Y, r_Y;
+                    for(const int& index_row : index) {
+                        if (X[index_row * this->number_of_cols + index_col] < threshold) {
+                            l_Y.push_back(Y[index_row]);
+                        }
+                        else {
+                            r_Y.push_back(Y[index_row]);
+                        }
+                    }
+                    double loss = (l_Y.size()/(double) index.size())*impurity(l_Y) + (r_Y.size()/(double) index.size())*impurity (r_Y);
+                    if ((loss < pnode->saved_loss) && (r_Y.size() >= lltree::min_size_split) && (l_Y.size() >= lltree::min_size_split)) {
+                        pnode->saved_loss = loss;
+                        pnode->saved_col_index = index_col;
+                        pnode->saved_threshold = threshold;
+                        pnode->isleaf = false;  
+                        pnode->l_size = l_Y.size();  
+                        pnode->r_size = r_Y.size();
+                    }
+                }  
+            }
+        }
+        if (!pnode->isleaf) {
+            unsigned int l_arr_idx = 0;
+            unsigned int r_arr_idx = 0;
+            std::vector<int> l_index(pnode->l_size);
+            std::vector<int> r_index(pnode->r_size);
+            for(const int& index_row : index) {
+                if (X[index_row * this->number_of_cols +  pnode->saved_col_index] < pnode->saved_threshold) {  
+                    l_index[l_arr_idx++] = index_row;
+                }
+                else {        
+                    r_index[r_arr_idx++] = index_row;
+                }
+            }            
+            node* l_node = new node(pnode->tree_level + 1);
+            node* r_node = new node(pnode->tree_level + 1);
+            pnode->set_children(l_node, r_node);
+            this->_grow(l_node, X, Y, l_index);
+            this->_grow(r_node, X, Y, r_index);
+        } 
+        else {
+            double average = 0;
+            for(const int& index_row : index) {average = average + Y[index_row];}                
+            pnode->leaf_value = average / index.size(); 
+            pnode->isleaf = true ;  
+        }  
+    }
+
+    double _traverse(node* pnode, double *row) {
+        if (pnode->isleaf){ return(pnode->leaf_value); }
+        
+        if (row[pnode->saved_col_index] < pnode->saved_threshold) {
+            return(_traverse(pnode->get_l_children(),row)); 
+        }
+        else {
+            return(_traverse(pnode->get_r_children(),row)); 
+        }
+    }
 public:
-    lltree() : max_depth(5), min_size_split(2) {} //default constructor to be remove (need at least a loss function).
-    //lltree(int max_depth, int min_size_split) : max_depth(max_depth), min_size_split(min_size_split) {}
-    // No Copy constructor (no need).
-    // No Copy assigment constructor (no need).
+    lltree() : max_depth(5), min_size_split(2) {} //default constructor
+
+   void fit(boost::python::numpy::ndarray const & np_X, boost::python::numpy::ndarray const & np_Y) {
+        // std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        double *X = reinterpret_cast<double *>(np_X.get_data());
+        double *Y = reinterpret_cast<double *>(np_Y.get_data());
+        this->number_of_cols = np_X.shape(1);
+        std::vector<int> index(np_X.shape(0));
+        int startValue = 0;
+        std:: generate (index.begin(), index.end(), [&](){return startValue++;});
+        this->_grow(&this->node_0, X, Y, index);
+        // std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();        
+        // std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+        // std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
+    }
+
+
+    boost::python::numpy::ndarray predict(boost::python::numpy::ndarray const & np_X) {
+        double *X = reinterpret_cast<double *>(np_X.get_data());        
+        std::vector<double> results;
+        for (int index_row = 0; index_row < np_X.shape(0); index_row ++){
+            results.push_back(_traverse(&this->node_0, &X[index_row * this->number_of_cols]));
+        }
+
+        // Py_intptr_t shape[1] = { (Py_intptr_t) results.size() };
+        // boost::python::numpy::ndarray result = boost::python::numpy::zeros(1, shape, boost::python::numpy::dtype::get_builtin<double>());
+        // std::copy(results.begin(), results.end(), reinterpret_cast<double*>(result.get_data()));
+        boost::python::numpy::ndarray result = boost::python::numpy::from_data(results.data(),  
+                                        boost::python::numpy::dtype::get_builtin<double>(),  
+                                        boost::python::make_tuple(results.size()), 
+                                        boost::python::make_tuple(sizeof(double)), 
+                                        boost::python::object());  
+        boost::python::numpy::ndarray output_array = result.copy();
+        return output_array;
+    } 
 
     // Utility method to get the address of the instance.
     uintptr_t get_address() const {
@@ -130,65 +155,12 @@ public:
         return this->tree_criterion.get(Y);
     }    
 
-    void fit(boost::python::numpy::ndarray const & np_X, boost::python::numpy::ndarray const & np_Y) {
-        double *X = reinterpret_cast<double *>(np_X.get_data());
-        double *Y = reinterpret_cast<double *>(np_Y.get_data());
-        std::vector<std::vector<double>> XY;
-        for (int i = 0; i < np_X.shape(0); i++) {
-            std::vector<double> row;
-            for (int j = 0; j < np_X.shape(1); j++) {row.push_back(X[i * np_X.shape(1) + j ]);} 
-            row.push_back(Y[i]);  
-            XY.push_back(row);
-        }
-        this->_grow(&this->node_0, XY);
-    }
-
-    boost::python::numpy::ndarray predict(boost::python::numpy::ndarray const & np_X) {
-        double *X_arr = reinterpret_cast<double *>(np_X.get_data());
-        std::vector<std::vector<double>> X;
-        for (int i = 0; i < np_X.shape(0); i++) {
-            std::vector<double> row;
-            for (int j = 0; j < np_X.shape(1); j++) {row.push_back(X_arr[i * np_X.shape(1) + j ]);} 
-            X.push_back(row);
-        }
-        
-        // double* results = new double[np_X.shape(0)];
-        std::vector<double> results;
-        for (size_t index_row = 0; index_row < np_X.shape(0); index_row ++){
-            double r = _traverse(&this->node_0, X[index_row]); 
-            results.push_back(r);
-            // results[index_row] = _traverse(&this->node_0, X[index_row]); 
-        }
-
-        Py_intptr_t shape[1] = { results.size() };
-        boost::python::numpy::ndarray result = boost::python::numpy::zeros(1, shape, boost::python::numpy::dtype::get_builtin<double>());
-        std::copy(results.begin(), results.end(), reinterpret_cast<double*>(result.get_data()));
-    
-        // boost::python::numpy::ndarray result = boost::python::numpy::from_data(results,  
-        //                                 boost::python::numpy::dtype::get_builtin<double>(),  
-        //                                 boost::python::make_tuple(X.size()), 
-        //                                 boost::python::make_tuple(sizeof(double)), boost::python::object());  
-        return result;
-    } 
-
     void print() {
         this->_print_tree(&this->node_0);
     }  
 
     void test() {
         std::cout << "1234" << std::endl;
-    }  
-
-
-    void print_XY(const std::vector<std::vector<double>> X) {
-        for (size_t i = 0; i < X.size(); i ++){
-            std::vector<double> row = X[i];
-            for (size_t j = 0; j < row.size(); j ++){std::cout << row[j] << " ";}
-            std::cout << std::endl;
-        }
-    }  
-    void print_XY_shape(const std::vector<std::vector<double>> X) {
-        std::cout << "[" << X.size() << "," << X[0].size() << "]" << std::endl;
     }  
 };
 
--- src/cpp/node.hpp
+++ src/cpp/node.hpp
@@ -17,6 +17,8 @@ public:
     node(int tree_level) : isleaf(true), tree_level(tree_level) {}
     bool isleaf;
     int tree_level;
+    int l_size;
+    int r_size;
     int saved_col_index = std::numeric_limits<int>::max();
     double saved_threshold = std::numeric_limits<double>::max();
     double saved_loss = std::numeric_limits<double>::max();
@@ -40,9 +42,11 @@ public:
         std::cout << "Node level : " << tree_level << std::endl;
         std::cout << "Node saved_loss : " << saved_loss << std::endl;
         std::cout << "Node saved_col_index : " << saved_col_index << std::endl;
-        std::cout << "Node saved_split : " << saved_threshold << std::endl;
+        std::cout << "Node saved_threshold : " << saved_threshold << std::endl;
         std::cout << "Node isleaf : " << std::boolalpha << isleaf << std::endl;
         std::cout << "Node leaf_value : " << leaf_value << std::endl;
+        std::cout << "Node l_size : " << l_size << std::endl;
+        std::cout << "Node r_size : " << r_size << std::endl;
         std::cout << "**********" << std::endl;
     }
 };
