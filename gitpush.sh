6afdf2b6050175441f05f3f4646e43c906b0e412
alexandreib <alexandre.durand.spam@gmail.com>
2024-04-13 21:05:34 +0800

    v1

--- gitpush.sh
+++ gitpush.sh
@@ -5,5 +5,5 @@ key=$(cat ~/pwd/git_key)
 echo 'Enter the commit message:'
 read commitMessage
 git commit -m "$commitMessage"
-git push  https://alexandreib:${key}@github.com/alexandreib/lltrees.git
+git push  https://alexandreib:${key}@github.com/alexandreib/lltrees_VHDL.git
 
